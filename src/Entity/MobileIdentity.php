f3daedb65a909050bb3387840e288117da902067
github-ci <you@example.com>
2025-04-28 09:43:25 +0000

    fix: 修复字段错误

--- src/Entity/MobileIdentity.php
+++ src/Entity/MobileIdentity.php
@@ -32,7 +32,7 @@ class MobileIdentity implements UserIdentityInterface
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[ListColumn]
     #[ORM\Column(length: 50, options: ['comment' => '手机号码'])]
